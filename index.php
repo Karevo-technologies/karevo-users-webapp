9725b689e1bc8e58b0d80a44d80d3cb4d7eaef9b
webdevcode-developer <developer.webdevcode@gmail.com>
2026-08-17 11:48:56 +0100

    Update index.php

--- index.php
+++ index.php
@@ -38,7 +38,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
 }
 
 // Error handling
-echo $_ENV['APP_URL'];
+print_r ($_ENV);
 set_exception_handler(function($e){
     http_response_code(500);
     echo json_encode([
